7ff6a870b17bd106e7bc7408092e8b4e0739e9bf
Willianson Araújo <willianson@users.noreply.github.com>
2024-07-15 15:24:53 -0300

    Add get product list.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const products = require('./products.json');
+
 const app = express();
 const port = 3000;
 
+app.get('/products', (request, response) => {
+  let findResult = products
+
+  response.send(findResult);
+});
 app.get('/', (request, response) => {
   response.send('Hello, World!');
 });
