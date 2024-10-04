5765b09493aaadd5ba92478312250fbf9b4c6a48
Willianson Araújo <willianson@users.noreply.github.com>
2024-10-04 20:55:58 -0300

    Beautify code.

--- api/index.js
+++ api/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const products = require('./products.json');
-
 const app = express();
 const port = 3000;
 
+
+/**
+ * GET Product by ID
+ * -----------------------------------------------------------------------------
+ */
+app.get('/products/:id', (request, response) => {
+  const findResult = products.find(product => product.id == request.params.id)
+
+  if (findResult === undefined) {
+    response.send([]);
+  } else {
+    response.send(findResult);
+  }
+});
+
+
+/**
+ * GET Products with Filters
+ * -----------------------------------------------------------------------------
+ */
 app.get('/products', (request, response) => {
   let findResult = products
 
@@ -40,15 +59,6 @@ app.get('/products', (request, response) => {
   response.send(findResult);
 });
 
-app.get('/products/:id', (request, response) => {
-  const findResult = products.find(product => product.id == request.params.id)
-
-  if (findResult === undefined) {
-    response.send([]);
-  } else {
-    response.send(findResult);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
